a27b5c0bbd24254c7c2cdca3fd9d332536c29878
AparmitSrivastava <aparmitsrivastava1@gmail.com>
2025-05-21 22:26:31 +0530

    working on mic

--- app/(main)/discussion-room/[roomid]/page.jsx
+++ app/(main)/discussion-room/[roomid]/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useParams } from 'next/navigation'
 import { useQuery } from 'convex/react'
 import { CoachingExpert } from '@/services/Options'
@@ -7,12 +7,19 @@ import { api } from '@/convex/_generated/api'
 import Image from 'next/image'
 import { UserButton } from '@stackframe/stack'
 import { Button } from '@/components/ui/button'
+// import RecordRTC from 'recordrtc'
+
 
 const page = () => {
   const { roomid } = useParams() //this how we extract the id 
   console.log(roomid);
   const DiscussionRoomData = useQuery(api.DiscussionRoom.GetDiscussionRoom, { id: roomid })
   const [expert, setexpert] = useState()
+  const [enableMic, setenableMic] = useState(false)
+  const recorder = useRef()
+  let silenceTimeout
+
+
 
   useEffect(() => {
     if (DiscussionRoomData) {
@@ -23,9 +30,55 @@ const page = () => {
   }, [DiscussionRoomData]) // if we dont use useeffect then error will come saying that expert is undef coz it takes time to call the data from diff file so we have to do this
 
 
+  const ConnectToServer = () => {
+    // CODE TO GET MICROPHONE ACCESS
+    setenableMic(true)
+    if (typeof window !== "undefined" && typeof navigator !== "undefined") {
+      navigator.mediaDevices.getUserMedia({ audio: true })
+        .then((stream) => {
+          recorder.current = new RecordRTC(stream, {
+            type: 'audio',
+            mimeType: 'audio/webm;codecs=pcm',
+            recorderType: RecordRTC.StereoAudioRecorder,
+            timeSlice: 250,
+            desiredSampRate: 16000,
+            numberOfAudioChannels: 1,
+            bufferSize: 4096,
+            audioBitsPerSecond: 128000,
+            ondataavailable: async (blob) => {
+              // if (!realtimeTranscriber.current) return;
+              // Reset the silence detection timer on audio input
+              clearTimeout(silenceTimeout);
+
+              const buffer = await blob.arrayBuffer();
+
+              //console.log(buffer)
+
+              // Restart the silence detection timer
+              silenceTimeout = setTimeout(() => {
+                console.log('User stopped talking');
+                // Handle user stopped talking (e.g., send final transcript, stop recording, etc.)
+              }, 2000);
+            },
+          });
+          recorder.current.startRecording();
+        })
+        .catch((err) => console.error(err));
+    }
+  }
+
+
+  const disconenct = (e)=>{
+    e.preventDefault();
+    recorder.current.pauseRecording()
+    recorder.current=null;
+    setenableMic(false)
+  }
+
+
 
   return (
-    <div>
+    <div className='-mt-12'>
       <h2 className="text-2xl font-bold">{DiscussionRoomData?.coachingOption}</h2>
       <div className='mt-5 grid grid-cols-1 lg:grid-cols-3 gap-10'>
 
@@ -52,19 +105,23 @@ const page = () => {
           </div>
 
           <div className='mt-5 flex justify-center items-center'>
-              <Button>Connect</Button>
+            {
+              !enableMic ? <Button onClick={ConnectToServer} >Connect</Button>
+                :
+                <Button variant='destructive' onClick={disconnect}>Disconnect</Button>
+            }
           </div>
 
 
 
         </div>
 
-
-        <div className='h-[60vh] border rounded-4xl bg-secondary flex flex-col items-center justify-center relative'>
-          <h2>Chat Section</h2>
-
+        <div>
+          <div className='h-[60vh] border rounded-4xl bg-secondary flex flex-col items-center justify-center relative'>
+            <h2>Chat Section</h2>
+          </div>
+          <h2 className='mt-5 text-gray-400 text-sm'>At the end of the sessiom we will automatically generate notes/feedback from your conversation</h2>
         </div>
-
 
 
       </div>
