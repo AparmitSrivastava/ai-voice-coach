dad8a2ff6e760e98bd48571d29311db4c238423f
AparmitSrivastava <aparmitsrivastava1@gmail.com>
2025-05-23 19:12:00 +0530

    content history added

--- app/(main)/dashboard/_components/Feedback.jsx
+++ app/(main)/dashboard/_components/Feedback.jsx
@@ -1,10 +1,64 @@
-import React from 'react'
+"use client"
+import { UserContext } from '@/app/_context/UserContext';
+import { Button } from '@/components/ui/button';
+import { api } from '@/convex/_generated/api';
+import { CoachingOptions } from '@/services/Options';
+import { useConvex } from 'convex/react'
+import Image from 'next/image';
+import React, { useContext, useEffect, useState } from 'react'
+import moment from 'moment';
+
+
 
 const Feedback = () => {
+  const convex = useConvex();
+  const { userData } = useContext(UserContext)
+  const [discussionRoomList, setdiscussionRoomList] = useState([])
+
+  useEffect(() => {
+    userData && GetDiscussionRooms()      // mthod is clled only when the user dat is available
+  }, [userData])
+
+
+
+
+
+  const GetDiscussionRooms = async () => {
+    const result = await convex.query(api.DiscussionRoom.GetAllDiscussionRoom, {
+      uid: userData?._id
+    })
+    console.log(result);
+    setdiscussionRoomList(result)
+  }
+
+  const GetAbstractImages = (option) => {
+    const coachingOption = CoachingOptions.find(item => item.name == option)
+    return coachingOption?.abstract ?? '/ab1.png';
+  }
+
+
   return (
-     <div>
-        <h2 className='font-bold text-xl text-black'>Feedback</h2>
-        <h2 className='text-gray-400'>You don't have any previous interview feedback</h2>
+    <div>
+      <h2 className='font-bold text-xl text-black'>Feedbacks</h2>
+      {discussionRoomList?.length == 0 && <h2 className='text-gray-400'>You don't have any previous feedback</h2>}
+
+      <div className='mt-5'>
+        {discussionRoomList.map((item, index) => (item.coachingOption == 'Mock Interview' || item.coachingOption == 'Ques Ans Prep') && (
+          <div key={index} className='border-b-[1px] pb-3 mb-4 group flex justify-center items-center cursor-pointer'>
+            <div className='flex gap-7 items-center'>
+              <Image src={GetAbstractImages(item.coachingOption)} alt='abstract'
+                width={50} height={50} className='rounded-full h-[50px] w-[50px]' />
+              <div>
+                <h2 className='font-bold'>{item.topic}</h2>
+                <h2 className='text-gray-400'>{item.coachingOption}</h2>
+                <h2 className='text-gray-400 text-sm'>{moment(item._creationTime).fromNow()}</h2>
+              </div>
+            </div>
+            <Button variant='outline' className='invisible group-hover:visible'>View Feedback</Button>
+          </div>
+        ))}
+      </div>
+
     </div>
   )
 }
