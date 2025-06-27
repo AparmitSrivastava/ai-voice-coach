1ed8f011b149578822315ed300dc7888697b9203
AparmitSrivastava <aparmitsrivastava1@gmail.com>
2025-06-27 20:08:00 +0530

    almost completed

--- app/(main)/discussion-room/[roomid]/_components/ChatBox.jsx
+++ app/(main)/discussion-room/[roomid]/_components/ChatBox.jsx
@@ -1,34 +1,3 @@
-// import React from 'react'
-
-// const Chatbox = ({ conversation }) => {
-//     return (
-//         <div>
-//             <div className="h-[60vh] border rounded-xl bg-secondary flex flex-col relative p-4 overflow-auto">
-                
-//                     {conversation.map((item, index) => (
-//                         <div key={`${item.role}-${index}-${item.content}`}  className={`flex ${item.role=='user' && 'justify-end'}`}>
-//                             {item?.role == "assistant" ?
-//                                 <h2 className='p-1 px-2 text-white mt-1 bg-primary inline-block rounded-md'>{item?.content}</h2>
-//                                 :
-//                                  <h2 className='p-1 px-2 mt-1 bg-gray-200 inline-block rounded-md'>{item?.content}</h2>
-//                             }
-
-//                         </div>
-//                     ))}
-                
-//             </div>
-//             <h2 className="mt-5 text-gray-400 text-sm">
-//                 At the end of the session we will automatically generate notes/feedback from your conversation
-//             </h2>
-//         </div>
-//     )
-// }
-
-// export default Chatbox
-
-
-
-
 import { AIModelToGenerateFeedbackAndNotes } from '@/services/GlobalServices'
 import {React , useState} from 'react'
 import { Button } from '@/components/ui/button'
